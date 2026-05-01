dc4731e839d66b06e505e636f48462e0b4b17db2
Starlight240 <milu.anemona@gmail.com>
2026-05-01 20:28:19 +0300

    Move project into a folder

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,28 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:camera_web/camera_web.dart';
+import 'package:firebase_auth_web/firebase_auth_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:geolocator_web/geolocator_web.dart';
+import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
+import 'package:permission_handler_html/permission_handler_html.dart';
+import 'package:sensors_plus/src/sensors_plus_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  CameraPlugin.registerWith(registrar);
+  FirebaseAuthWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  GeolocatorPlugin.registerWith(registrar);
+  GoogleMapsPlugin.registerWith(registrar);
+  WebPermissionHandler.registerWith(registrar);
+  WebSensorsPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
